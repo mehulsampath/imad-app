730f4f2ec92a03fd6cc4360acd780863d4f86882
mehulsampath <30820943+mehulsampath@users.noreply.github.com>
2017-08-13 01:17:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+
+//chanage the text of the main-text division
+
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'new value';
+
+//move the image
+var img = document.getElementById('ig');
+log.onclick = function() {
+    log.style.marginLeft = '100px';
+};
