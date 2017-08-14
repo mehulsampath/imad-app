d503ba14c59d9a3f92da5db5d5a18b85e4265347
mehulsampath <30820943+mehulsampath@users.noreply.github.com>
2017-08-14 23:17:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,19 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
 
-//chanage the text of the main-text division
-
-
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft += 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-     var interval = setInterval(moveRight, 100);
+button.onclick = function() {
+  var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function() {
+   if(request.readyState === XMMLHttpRequest.Done) {
+       if (request.status === 200) {
+           var counter = request.responseText;
+           var span = document.getElementById('count');
+           span.innerHTML = counter.toString();
+       }
+   }
+  };
+  
+  request.open('GET', 'http://msampath21.imad.hasura-app.io/counter', true);
+  request.send(null);
+  
 };
