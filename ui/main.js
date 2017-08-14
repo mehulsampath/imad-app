81bd4cc856e9d041c8dacb02cecb4d1bd1ded7c5
mehulsampath <30820943+mehulsampath@users.noreply.github.com>
2017-08-14 22:51:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,7 @@ console.log('Loaded!');
 
 //chanage the text of the main-text division
 
-var element = document.getElementById('main-text');
 
-element.innerHTML = 'new value';
 
 //move the image
 var img = document.getElementById('madi');
