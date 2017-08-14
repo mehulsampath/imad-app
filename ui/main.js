5bd75a16e206a9a4865b221aeab4f2e68e37f041
mehulsampath <30820943+mehulsampath@users.noreply.github.com>
2017-08-14 23:22:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ button.onclick = function() {
   var request = new XMLHttpRequest();
   
   request.onreadystatechange = function() {
-   if(request.readyState === XMMLHttpRequest.Done) {
+   if(request.readyState === XMLHttpRequest.Done) {
        if (request.status === 200) {
            var counter = request.responseText;
            var span = document.getElementById('count');
