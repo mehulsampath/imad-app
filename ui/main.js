71765c74b991c092a329d91a3780548a7f9ee7c6
mehulsampath <30820943+mehulsampath@users.noreply.github.com>
2017-08-15 00:13:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,18 @@ button.onclick = function() {
   request.send(null);
   
 };
+
+var nameInput =document.getElementById('name');
+var name = nameinput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    
+    
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var = i, i < names.length, i++) {
+        list += '<li>' + nammes[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+}
