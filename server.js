ba6b13a7f77ffbe8653db3f7df8075c05f157149
mehulsampath <30820943+mehulsampath@users.noreply.github.com>
2017-08-22 21:53:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -17,18 +19,17 @@ res.send(counter.toString());
 
 
 });
-app.get('/article-one', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
 
-app.get('/article-two', function(req, res) {
-    res.send('Article two requested and will be served here');
-});
+function  hash(input, salt) {
+    var hashed = crypto.pbkd25Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
 
-app.get('/article-three', function(req, res) {
-    res.send('Article three requested and will be served here');
-});
+app.get('/hash:input', function(req, res){
+ var hashedString = hash(req.params.input, 'this is some random string');
+ res.send(hashedString);
 
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
