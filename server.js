3e65ee447cff4095047acabfd8431982f854f41e
mehulsampath <30820943+mehulsampath@users.noreply.github.com>
2017-08-22 22:34:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
 var crypto = require('crypto');
+var bodyParser = require('body_parser');
 
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -29,6 +33,26 @@ app.get('/hash/:input', function(req, res){
  var hashedString = hash(req.params.input, 'this is some random string');
  res.send(hashedString);
 
+});
+
+app.post('create-user', function(req, res) {
+    
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
+        
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(' User succesfully created: ' + username);
+        }
+        
+    });
+    
 });
 
 app.get('/ui/style.css', function (req, res) {
